d37bd335dc7b38640ff76c97ef6c9500aa6944f3
Danny Morrow <danny@unitytheory.com>
2016-11-11 12:05:58 -0500

    additional nsmanagedobject convenience methods

--- Classes/CoreData/NSManagedObject+UT.swift
+++ Classes/CoreData/NSManagedObject+UT.swift
@@ -13,4 +13,11 @@ extension NSManagedObject {
     open static var entityName:String {
         return String(describing: self)
     }
+    open class func fetchRequest<T:NSManagedObject>() -> NSFetchRequest<T> {
+        return NSFetchRequest<T>(entityName: T.entityName);
+    }
+    
+    open static func description(in context: NSManagedObjectContext) -> NSEntityDescription? {
+        return NSEntityDescription.entity(forEntityName: self.entityName, in: context)
+    }
 }
