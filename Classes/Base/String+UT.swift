a46bb15d29686ab4e7f1b08cd6469451a2dbd88a
Danny Morrow <danny@unitytheory.com>
2017-01-12 21:52:53 -0500

    string nsrange->range and range->nsrange

--- Classes/Base/String+UT.swift
+++ Classes/Base/String+UT.swift
@@ -69,6 +69,25 @@ extension String {
         return linkString
     }
     
+    //https://gist.github.com/robnadin/2720534f91702c444b6b9bde0fdfe224
+    
+    public func nsRange(from range: Range<String.Index>) -> NSRange {
+        let from = range.lowerBound.samePosition(in: utf16)
+        let to = range.upperBound.samePosition(in: utf16)
+        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from),
+                       length: utf16.distance(from: from, to: to))
+    }
+    
+    public func range(from nsRange: NSRange) -> Range<String.Index>? {
+        guard
+            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
+            let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self)
+            else { return nil }
+        return from ..< to
+    }
+    
     //Substrings - http://stackoverflow.com/a/39742687/1189470
     
     func substring(from: Int?, to: Int?) -> String {
