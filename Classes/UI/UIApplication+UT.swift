8c1b6c20cb455167596d34e885a9c36bc2088f4c
Danny Morrow <danny@unitytheory.com>
2017-08-11 10:30:29 -0400

    Get top view controller anywhere

--- Classes/UI/UIApplication+UT.swift
+++ Classes/UI/UIApplication+UT.swift
@@ -89,6 +89,21 @@ extension UIApplication {
         return className
     }
     
+    open class func topViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+    
     open class var ruleThickness:CGFloat {
         return  1.0/UIDevice.screenScale
     }
